d2c374ece519f8a635649ce7defd2eb2ef058f7b
taufiksoleh <taufik.soleh@bukalapak.com>
2023-11-26 23:27:04 +0700

    Add dependencies and classes for Hibernate Reactive Panache and Fruit entity

---
+++ src/main/java/org/acme/hibernate/orm/panache/Fruit.java
@@ -0,0 +1,14 @@
+package org.acme.hibernate.orm.panache;
+
+import jakarta.persistence.Cacheable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+@Cacheable
+class Fruit extends BaseEntity {
+
+    @Column(length = 40, unique = true)
+    public String name;
+
+}
